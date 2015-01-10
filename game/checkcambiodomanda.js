1a7cafc8f9c1c60761d6aa402678eb06d4ce1515
saurogabellini <sgabe76@gmail.com>
2015-01-10 15:45:15 +0100

    Controllo Invii Multipli
    Controllo Invii multipli, chat domande sort..

--- game/checkcambiodomanda.js
+++ game/checkcambiodomanda.js
@@ -23,14 +23,31 @@
 		if (data == 'N' && pagina != data) {
 			document.location.href = 'playtastieranumerica.htm';
 		}
- 
+		if (data == 'S3' && pagina != data) {
+			document.location.href = 'playsort3.htm';
+		}
+		if (data == 'M3' && pagina != data) {
+			document.location.href = 'playmatch3.htm';
+		}
+		if (data == 'S4' && pagina != data) {
+			document.location.href = 'playsort4.htm';
+		}
+		if (data == 'M4' && pagina != data) {
+			document.location.href = 'playmatch4.htm';
+		}
+		if (data == 'S5' && pagina != data) {
+			document.location.href = 'playsort5.htm';
+		}
+		if (data == 'M5' && pagina != data) {
+			document.location.href = 'playmatch5.htm';
+		}
 }
 
 
 function CambioPagina(pagina) {
 
     var doStuff = function() {
-          
+        RicezioneOk = 0;
 
         var uri = 'http://' + serverip + '/cambiapagina.ashx';
 
